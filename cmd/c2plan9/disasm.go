18c34edd5f3ed5229f96257cbf2e7dd92293b327
Lemon4ksan <arsenii.komolov@yandex.ru>
2026-08-25 20:49:26 +0300

    fix(c2plan9): support 64-bit CMOVQ mnemonics and precise 8-bit register operand decoding

--- cmd/c2plan9/disasm.go
+++ cmd/c2plan9/disasm.go
@@ -107,6 +107,27 @@ func DisassembleAMD64(code []byte, baseOffset uint64) ([]DisassembledInst, error
 			}
 		}
 
+		// Fix x86asm GoSyntax bug where 8-bit registers (R8B..R15B, AL..DL, SIL, DIL) lose their 'B' and become CMPL/TESTL/MOVL/ANDL
+		for _, arg := range inst.Args {
+			if reg, ok := arg.(x86asm.Reg); ok {
+				regStr := reg.String()
+				if is8BitRegStr(regStr) {
+					baseReg := strings.TrimSuffix(regStr, "B")
+					if strings.HasPrefix(text, "CMPL ") {
+						text = "CMPB " + text[5:]
+					} else if strings.HasPrefix(text, "TESTL ") {
+						text = "TESTB " + text[6:]
+					} else if strings.HasPrefix(text, "MOVL ") {
+						text = "MOVB " + text[5:]
+					} else if strings.HasPrefix(text, "ANDL ") {
+						text = "ANDB " + text[5:]
+					}
+					text = strings.ReplaceAll(text, " "+baseReg+",", " "+regStr+",")
+					text = strings.ReplaceAll(text, ", "+baseReg, ", "+regStr)
+				}
+			}
+		}
+
 		insts = append(insts, DisassembledInst{
 			Offset:   pc,
 			Length:   inst.Len,
@@ -151,6 +172,50 @@ func cleanPlan9Syntax(s string) string {
 		s = "NOP"
 	}
 
+	// Normalize 8-bit registers for SETcc instructions
+	if strings.HasPrefix(s, "SET") {
+		parts := strings.SplitN(s, " ", 2)
+		if len(parts) == 2 {
+			op := parts[0]
+			reg := strings.TrimSpace(parts[1])
+			switch reg {
+			case "AX":
+				reg = "AL"
+			case "BX":
+				reg = "BL"
+			case "CX":
+				reg = "CL"
+			case "DX":
+				reg = "DL"
+			case "SI":
+				reg = "SIL"
+			case "DI":
+				reg = "DIL"
+			case "BP":
+				reg = "BPL"
+			case "SP":
+				reg = "SPL"
+			case "R8":
+				reg = "R8B"
+			case "R9":
+				reg = "R9B"
+			case "R10":
+				reg = "R10B"
+			case "R11":
+				reg = "R11B"
+			case "R12":
+				reg = "R12B"
+			case "R13":
+				reg = "R13B"
+			case "R14":
+				reg = "R14B"
+			case "R15":
+				reg = "R15B"
+			}
+			s = op + " " + reg
+		}
+	}
+
 	// Fix 8-bit register suffixes/prefixes (e.g. CMPL DL, $160 -> CMPB DL, $160)
 	eightBitRegs := []string{"AL", "BL", "CL", "DL", "SIL", "DIL", "BPL", "SPL", "R8B", "R9B", "R10B", "R11B", "R12B", "R13B", "R14B", "R15B"}
 	for _, reg := range eightBitRegs {
@@ -181,6 +246,22 @@ func cleanPlan9Syntax(s string) string {
 		}
 	}
 
+
+
+	sixtyFourBitRegs := []string{"AX", "BX", "CX", "DX", "SI", "DI", "BP", "SP", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15"}
+	is64Dest := func(str string) bool {
+		parts := strings.Split(str, ",")
+		if len(parts) >= 2 {
+			dest := strings.TrimSpace(parts[len(parts)-1])
+			for _, r := range sixtyFourBitRegs {
+				if dest == r {
+					return true
+				}
+			}
+		}
+		return false
+	}
+
 	// Mnemonic translations for Go's Plan 9 assembler
 	if strings.HasPrefix(s, "MOVZX ") {
 		s = "MOVBLZX " + s[6:]
@@ -189,25 +270,65 @@ func cleanPlan9Syntax(s string) string {
 	} else if strings.HasPrefix(s, "MOVSXD ") {
 		s = "MOVLQSX " + s[7:]
 	} else if strings.HasPrefix(s, "CMOVAE ") {
-		s = "CMOVLCC " + s[7:]
+		if is64Dest(s) {
+			s = "CMOVQCC " + s[7:]
+		} else {
+			s = "CMOVLCC " + s[7:]
+		}
 	} else if strings.HasPrefix(s, "CMOVB ") {
-		s = "CMOVLCS " + s[6:]
+		if is64Dest(s) {
+			s = "CMOVQCS " + s[6:]
+		} else {
+			s = "CMOVLCS " + s[6:]
+		}
 	} else if strings.HasPrefix(s, "CMOVE ") {
-		s = "CMOVLEQ " + s[6:]
+		if is64Dest(s) {
+			s = "CMOVQEQ " + s[6:]
+		} else {
+			s = "CMOVLEQ " + s[6:]
+		}
 	} else if strings.HasPrefix(s, "CMOVNE ") {
-		s = "CMOVLNE " + s[7:]
+		if is64Dest(s) {
+			s = "CMOVQNE " + s[7:]
+		} else {
+			s = "CMOVLNE " + s[7:]
+		}
 	} else if strings.HasPrefix(s, "CMOVG ") {
-		s = "CMOVLGT " + s[6:]
+		if is64Dest(s) {
+			s = "CMOVQGT " + s[6:]
+		} else {
+			s = "CMOVLGT " + s[6:]
+		}
 	} else if strings.HasPrefix(s, "CMOVGE ") {
-		s = "CMOVLGE " + s[7:]
+		if is64Dest(s) {
+			s = "CMOVQGE " + s[7:]
+		} else {
+			s = "CMOVLGE " + s[7:]
+		}
 	} else if strings.HasPrefix(s, "CMOVL ") {
-		s = "CMOVLLT " + s[6:]
+		if is64Dest(s) {
+			s = "CMOVQLT " + s[6:]
+		} else {
+			s = "CMOVLLT " + s[6:]
+		}
 	} else if strings.HasPrefix(s, "CMOVLE ") {
-		s = "CMOVLLE " + s[7:]
+		if is64Dest(s) {
+			s = "CMOVQLE " + s[7:]
+		} else {
+			s = "CMOVLLE " + s[7:]
+		}
 	} else if strings.HasPrefix(s, "CMOVNS ") {
-		s = "CMOVLPL " + s[7:]
+		if is64Dest(s) {
+			s = "CMOVQPL " + s[7:]
+		} else {
+			s = "CMOVLPL " + s[7:]
+		}
 	} else if strings.HasPrefix(s, "CMOVS ") {
-		s = "CMOVLMI " + s[6:]
+		if is64Dest(s) {
+			s = "CMOVQMI " + s[6:]
+		} else {
+			s = "CMOVLMI " + s[6:]
+		}
 	} else if strings.HasPrefix(s, "TZCNT ") {
 		s = "TZCNTL " + s[6:]
 	} else if strings.HasPrefix(s, "BSF ") {
@@ -232,7 +353,23 @@ func cleanPlan9Syntax(s string) string {
 		s = "SETHI " + s[5:]
 	} else if strings.HasPrefix(s, "SETBE ") {
 		s = "SETLS " + s[6:]
+	} else if strings.HasPrefix(s, "SETL ") {
+		s = "SETLT " + s[5:]
+	} else if strings.HasPrefix(s, "SETG ") {
+		s = "SETGT " + s[5:]
+	} else if strings.HasPrefix(s, "SETNE ") {
+		s = "SETNE " + s[6:]
 	}
 
 	return s
+}
+
+func is8BitRegStr(r string) bool {
+	switch r {
+	case "AL", "BL", "CL", "DL", "AH", "BH", "CH", "DH",
+		"SIL", "DIL", "BPL", "SPL",
+		"R8B", "R9B", "R10B", "R11B", "R12B", "R13B", "R14B", "R15B":
+		return true
+	}
+	return false
 }
