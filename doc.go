dc438670455e28754a597222f5c4fed31d3baea4
Lemon4ksan <arsenii.komolov@yandex.ru>
2026-08-27 21:29:54 +0300

    fix: add doc.go file so that the benchmarks can be located

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2026 Lemon4ksan All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package foundation provides a unified, zero-allocation foundational networking and computing runtime for Go.
+package foundation
