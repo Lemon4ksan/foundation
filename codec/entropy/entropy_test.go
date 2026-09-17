59f1419e69b5682e549b0500ee2caec4c298166c
Lemon4ksan <arsenii.komolov@yandex.ru>
2026-09-17 09:19:59 +0300

    fix(codec/entropy): resolve internal test namespace

--- codec/entropy/entropy_test.go
+++ codec/entropy/entropy_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package entropy_test
+package entropy
 
 import (
 	"crypto/rand"
@@ -34,4 +34,5 @@ func TestShannonEntropy(t *testing.T) {
 		t.Errorf("expected text sample to not be incompressible")
 	}
 }
+
 
